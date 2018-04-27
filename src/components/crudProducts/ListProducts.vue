8073d58ac7f9f35d14d49d717a2063d55bcad7fd
Francine Moraes <francinemoraes08@hotmail.com>
2018-04-27 10:35:21 -0300

    Adding files

---
+++ src/components/crudProducts/ListProducts.vue
@@ -0,0 +1,53 @@
+<template>
+  <table>
+      <tr>
+          <th>Nome</th>
+          <th>Código de Barras</th>
+          <th>Quantidade em estoque</th>
+          <th>Valor unitário</th>
+      </tr>
+      <tr v-for="prod in produtos" :key="prod.nome">
+          <td>{{prod.nome}}</td>
+          <td>{{prod.codigoDeBarra}}</td>
+          <td>{{prod.estoque}}</td>
+          <td>{{prod.valorUn}}</td>
+      </tr>
+  </table>
+</template>
+
+
+
+
+<script>
+export default {
+  data(){
+      return{
+          produtos:[]
+      }
+  },
+
+  created: function() {
+      this.$http.get("http://localhost:8080/produto").then(function(response){
+          this.produtos = response.body;
+      }, function(response){
+          console.log("nao funcionou")
+      })
+  }
+}
+</script>
+
+
+<style scoped>
+    table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 50%;
+    margin: auto;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+</style>
