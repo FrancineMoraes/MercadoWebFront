10eaf063b429e2476c8b0139072f6a8d4b1e999b
Francine Moraes <francinemoraes08@hotmail.com>
2018-04-30 12:28:36 -0300

    missing small settings

--- src/components/crudProducts/CreateProducts.vue
+++ src/components/crudProducts/CreateProducts.vue
@@ -1,24 +1,69 @@
 <template>
-  <div id="formup" class="create">
+  <div class="formup">
     <h1>Cadastro de Produtos</h1>
-      <form class="createform">
-    	        <input type="text" id="desc" name="" placeholder="Descrição do produto" required="required" /><br>
-                <input type="password" id="qtd" name="" placeholder="Quantidade" required="required" /><br>
-                <input type="text" id="valor" name="" placeholder="Valor unitário"/> <br>
-                <button type="submit" class="btn btn-primary btn-block btn-large">Enviar</button>
+      <form class="createform" v-on:submit.prevent="insert">
+    	        <input  type="text" v-model="produto.codigoDeBarra" 
+              placeholder="Código de Barra" required="required" />
+              <br>
+
+              <input type="text" v-model="produto.estoque" 
+              placeholder="Quantidade" required="required" />
+              <br>
+
+              <input type="text" v-model="produto.nome" 
+              placeholder="Descrição do produto" required="required" />
+              <br>
+              
+              <input type="text" v-model="produto.valorUn" 
+              placeholder="Valor unitário"/> 
+              <br>
+
+              <button type="submit" class="btn btn-primary btn-block btn-large"
+              >Enviar</button>
       </form>
   </div>
 </template>
 
 <script>
+//const url = 'https://5ae67b7736a18b00144e39a8.mockapi.io/produtos'
+const url = "http://localhost:8080"
 export default {
-  name: 'CreateProducts'
-}
+
+  data(){
+      return{
+        produto:{
+            codigoDeBarra: '',
+            estoque: '',
+            nome: '',
+            valorUn: ''
+      }
+      }
+    },
+
+
+  name: 'CreateProducts',
+
+
+  methods: {
+
+      insert(){
+          console.log(this.produto)
+          
+          this.$http.post(url, this.produto).then(function(response){
+          console.log(response, " funcionou");
+      }, function(error){
+          console.log(error, " nao funcionou")
+      });
+      }
+  }
+  }
+
+
 </script>
 
 <style scoped>
-  #formup{
-    margin: 0 auto 0 auto;
+  .formup{
+    margin: 13% auto 0 auto;
   }
 
   input{
